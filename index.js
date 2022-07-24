2eb3e00ea6f6df5f63c4d59d9885a5e376424ef2
Freddie Brewin <freddiebrewin@live.com>
2022-07-24 20:33:39 +0100

    commit

---
+++ index.js
@@ -0,0 +1,59 @@
+let isModalOpen = false;
+let constrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    console.log(x, y)
+    for(let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const booleanInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * booleanInt}px, ${y * booleanInt}px) rotate(${x / scaleFactor}deg)`
+        // shapes[i].style.transform = `rotate(${x}deg)`
+    }
+}
+
+function toggleContrast() {
+    constrastToggle = !constrastToggle
+    if(constrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    
+    emailjs
+        .sendForm(
+            'service_zgpkgjw',
+            'template_cm4wn4j',
+            event.target,
+            'cwI6VZszb2Lg7X3fo'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+        console.log('it worked 1')
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on freddiebrewin@gmail.com"
+        );
+    })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal__open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal__open"
+}
